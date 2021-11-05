ec147c429c23a5403ba9389b7fcbfdc665aa770a
Paulo Ricardo <pauloricar.adm@gmail.com>
2021-11-04 22:41:49 -0300

    main

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const buttonSend = document.querySelector("#send");
 
 function startGame() {
 
-
-
-
-
     buttonSend.addEventListener("click", function (e) {
         e.preventDefault();
 
@@ -19,12 +15,28 @@ function startGame() {
         if (valueCardsGame < 4 || valueCardsGame > 14 || resto !== 0) {
             alert("Veja a Regra sobre o número de cartas")
         }
-
-
-
-
+        else {
+            alert("Ebaaa, vamos começar!")
+            numberCards(valueCardsGame)
+        }
     })
 }
-
-
 startGame()
+
+
+function numberCards(valueCardsGame) {
+    let contador = 0;
+
+    while (contador < valueCardsGame) {
+
+        let carta = document.querySelector(".cards-game");
+        carta.innerHTML =
+            `<li>
+                <div class="card">
+                    <img src="/assets/front.png" alt="">
+                </div>
+            </li>`
+            + carta.innerHTML;
+        contador++;
+    }
+}
