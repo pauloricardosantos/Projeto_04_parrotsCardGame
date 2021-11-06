a06c9b1cb42c0fdccfdd67180bf74f50024394e7
Paulo Ricardo <pauloricar.adm@gmail.com>
2021-11-05 22:20:51 -0300

    Adicionado os gifs dentro dos cards e ajustado o tamanho das faces e do main para melhor posicionamento

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
 const buttonSend = document.querySelector("#send");
+let valueName = ""
+let valueCardsGame = 0
 
-function startGame() {
+const parrolCards = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"]
 
-    buttonSend.addEventListener("click", function (e) {
-        e.preventDefault();
+let choosedCards = [];
 
-        const inputName = document.querySelector(".inputName");
-        let valueName = inputName.value
+buttonSend.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const cardsGame = document.querySelector(".inputCardsGame");
-        let valueCardsGame = cardsGame.value;
-        const resto = (valueCardsGame % 2)
+    const inputName = document.querySelector(".inputName");
+    valueName = inputName.value
 
-        if (valueCardsGame < 4 || valueCardsGame > 14 || resto !== 0) {
-            alert("Veja a Regra sobre o número de cartas")
-        }
-        else {
-            alert("Ebaaa, vamos começar!")
-            numberCards(valueCardsGame)
-        }
-    })
-}
-startGame()
+    const cardsGame = document.querySelector(".inputCardsGame");
+    valueCardsGame = cardsGame.value;
+    const resto = (valueCardsGame % 2)
+
+    if (valueCardsGame < 4 || valueCardsGame > 14 || resto !== 0) {
+        alert("Veja a Regra sobre o número de cartas")
+    }
+    else {
+        alert("Preparado " + valueName + "? \n Vamos começar!!!!")
+        sendCard()
+    }
+})
 
 
-function numberCards(valueCardsGame) {
-    let contador = 0;
-
-    while (contador < valueCardsGame) {
-
-        let carta = document.querySelector(".cards-game");
-        carta.innerHTML =
+function sendCard() {
+    mix()
+    for (let i = 0; i < (valueCardsGame / 2); i++) {
+        choosedCards.push(parrolCards[i]);
+        choosedCards.push(parrolCards[i]);
+    }
+    choosedCards.sort(comparator);
+    const card = document.querySelector(".cards-game");
+    for (let i = 0; i < valueCardsGame; i++) {
+        card.innerHTML +=
             `<li>
-                <div class="card">
-                    <img src="/assets/front.png" alt="">
+                <div data-identifier="card" class="card">
+                    <div data-identifier="front-face" class="front-face face">
+                        <img src="/assets/front.png" alt="">
+                    </div>
+                    <div data-identifier="back-face" class="back-face face">
+                    <img src="/assets/${choosedCards[i]}.gif" alt="">
+                    </div>
                 </div>
             </li>`
-            + carta.innerHTML;
-        contador++;
+        const welcome = document.querySelector('.container-apresentacao')
+        welcome.classList.add('hidden')
     }
-
-    const apresentacao = document.querySelector('.container-apresentacao')
-    apresentacao.classList.add('hidden')
+}
+function mix() {
+    parrolCards.sort(comparator)
+}
+function comparator() {
+    return Math.random() - 0.5;
 }
